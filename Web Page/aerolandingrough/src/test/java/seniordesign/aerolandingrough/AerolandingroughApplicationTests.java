734ceac93fad4286710c2e9301d8c43bc575dbe0
Troy Rana <121784171+Anaryort@users.noreply.github.com>
2025-09-25 06:53:45 -0700

    Webpage Template
    - Create folder for organization
    - Folder contains Maven-Springboot project with dependencies  as a baseline

---
+++ Web Page/aerolandingrough/src/test/java/seniordesign/aerolandingrough/AerolandingroughApplicationTests.java
@@ -0,0 +1,13 @@
+package seniordesign.aerolandingrough;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerolandingroughApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
